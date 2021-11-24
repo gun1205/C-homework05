036a0c9bc30e34422f3a1849474e95691f95b798
patrick1205 <patrick.tw2002@gmail.com>
2021-11-25 01:09:06 +0800

    first

---
+++ p08/main.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#define SIZE 10
+
+int main(void)
+{
+	int i;
+	int j;
+	int a[SIZE] = { 19, 3, 15, 7, 11, 9, 13, 5, 17, 1 };
+
+	printf("%s%13s%17s\n", "element", "value", "histogram");
+
+	for (i = 0; i < SIZE; i++)
+	{
+		printf("%7d%13d  ", i, a[i]);
+		for (j = 1; j <=a[i]; j++)
+		{
+			printf("%c", '*');
+		}
+		printf("\n");
+	}
+	system("pause");
+	return 0;
+}
---
+++ p13/source/main.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void printarray(const int a[][3]);
+
+int main(void)
+{
+	int array1[2][3] = { {1,2,3}, {4,5,6} };
+	int array2[2][3] = { 1,2,3,4,5 };
+	int array3[2][3] = { {1,2}, {4} };
+
+	printf("values in array1 by row are : \n");
+	printarray(array1);
+
+	printf("values in array2 by row are : \n");
+	printarray(array2);
+
+	printf("values in array3 by row are : \n");
+	printarray(array3);
+
+	system("pause");
+	return 0;
+}
+
+void printarray(const int a[][3])
+{
+	
+	int i, j;
+
+	for (i = 0; i < 2; i++)
+	{
+		for (j = 0; j < 3; j++)
+		{
+			printf("%d ", a[i][j]);
+		}
+		printf("\n");
+	}
+}
